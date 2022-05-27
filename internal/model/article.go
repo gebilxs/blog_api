4e327e202741aa7e07907194f3ed4aa52847c153
gebilxs <1799676669@qq.com>
2022-05-27 23:14:52 +0800

    router_tag_article

---
+++ internal/model/article.go
@@ -0,0 +1,14 @@
+package model
+
+type Article struct {
+	*Model               //类似于某种继承
+	Title         string `json:"title"`
+	Desc          string `json:"desc"`
+	Content       string `json:"content"`
+	CoverImageUrl string `json:"cover_image_url"`
+	State         uint8  `json:"state"`
+}
+
+func (a Article) TableName() string {
+	return "blog_article" //返回数据表
+}
