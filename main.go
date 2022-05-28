fcbc6db44b5b848ae6794ce29e667639fa434019
gebilxs <1799676669@qq.com>
2022-05-28 12:15:45 +0800

    add config

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"blog_api/global"
 	"blog_api/internal/routers"
+	"blog_api/pkg/setting"
+	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 	"time"
 )
 
+func init() {
+	err := setupSettings()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 	s := &http.Server{
 		Addr:           ":8080",
@@ -16,4 +27,29 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
+}
+
+//初始化项目读取
+
+func setupSettings() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+
 }
---
+++ pkg/setting/setting.go
@@ -0,0 +1,20 @@
+package setting
+
+import "github.com/spf13/viper"
+
+type Setting struct {
+	vp *viper.Viper
+}
+
+func NewSetting() (*Setting, error) {
+	vp := viper.New()
+	vp.SetConfigName("config")
+	//设置相关的路径
+	vp.AddConfigPath("config/")
+	vp.SetConfigType("yaml")
+	err := vp.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+	return &Setting{vp}, nil
+}
